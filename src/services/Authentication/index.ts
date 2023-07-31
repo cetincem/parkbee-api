409eab4312728da271b230af2e9014879e4c5f09
Cetin Cem <cetincem.atabey@gmail.com>
2023-07-31 17:19:00 +0300

    Create Authentication service

---
+++ src/services/Authentication/index.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import querystring from 'querystring';
+
+import { RequestAccessTokenParams, RequestAccessTokenResponse } from "./types";
+
+class AuthenticationService {
+  private authUrl: string;
+
+  constructor(authUrl: string) {
+    this.authUrl = authUrl;
+  }
+
+  async requestAccessToken(params: RequestAccessTokenParams): Promise<RequestAccessTokenResponse> {
+    try {
+      const data = {
+        client_id: params.client_id,
+        client_secret: params.client_secret,
+        grant_type: params.grant_type,
+        scope: params.scope,
+      };
+      const response: AxiosResponse = await axios.post(this.authUrl, querystring.stringify(data));
+      return response.data as RequestAccessTokenResponse;
+    } catch (err) {
+      throw this.handleAuthError(err);
+    }
+  }
+
+  private handleAuthError(err: any) {
+    if (axios.isAxiosError(err)) {
+      const error = err as AxiosError;
+      return error.response?.data;
+    } else return new Error('Authentication failed');
+  }
+}
+
+export default AuthenticationService;
