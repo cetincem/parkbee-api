b0e51d1a3086ed3b4f2090ba88f6558b2542f43e
Cetin Cem <cetincem.atabey@gmail.com>
2023-08-02 13:30:03 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import AuthenticationService from './src/services/Authentication';
+import GaragesService from './src/services/Garages';
+import UsersService from './src/services/Users';
+import PrepaidBookingsService from './src/services/PrepaidBookings';
+import PayPerMinuteParkingService from './src/services/PayPerMinuteParking';
+
+export default {
+  AuthenticationService,
+  GaragesService,
+  UsersService,
+  PrepaidBookingsService,
+  PayPerMinuteParkingService,
+};
