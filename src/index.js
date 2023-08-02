98412e73cca37022f2e66a163a535b8062829943
Cetin Cem <cetincem.atabey@gmail.com>
2023-08-02 13:49:24 +0300

    Move index.js into src folder

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import AuthenticationService from './services/Authentication';
+import GaragesService from './services/Garages';
+import UsersService from './services/Users';
+import PrepaidBookingsService from './services/PrepaidBookings';
+import PayPerMinuteParkingService from './services/PayPerMinuteParking';
+
+export default {
+  AuthenticationService,
+  GaragesService,
+  UsersService,
+  PrepaidBookingsService,
+  PayPerMinuteParkingService,
+};
